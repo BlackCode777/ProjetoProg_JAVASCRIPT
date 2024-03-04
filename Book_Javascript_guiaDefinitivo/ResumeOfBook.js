921a6edd3f1b3d51fef9aa1fe1816efb1ff280d9
Anderson Martins <BlackCode77.1980@gmail.com>
2024-03-04 00:58:08 -0300

    [ Javascript ] - atualizando

--- Book_Javascript_guiaDefinitivo/ResumeOfBook.js
+++ Book_Javascript_guiaDefinitivo/ResumeOfBook.js
@@ -1,10 +1,126 @@
+// Data: 2024-03-03
+
+// Objeto Site
+let Site = {
+    nome = String,
+    url = String,
+    categoria = String,
+}
+
+function FuncConstSite() {
+    this.nome = "Google";
+    this.url = "www.google.com";
+    this.categoria = "Buscador";
+}
+
+let site = new FuncConstSite();
+site.nome;
+site.url;
+site.categoria;
+console.log(site.nome);
+console.log(site.url);
+console.log(site.categoria);
+
+// Objeto pessoa
+let Pessoa{
+    nome = String,
+    idade = Number,
+    profissao = String,
+}
+
+function FuncConstPessoa() {
+    this.nome = "Lucas";
+    this.idade = 20;
+    this.profissao = "Pedreiro";
+}
+
+// Chamando a função construtora
+let pessoa = new FuncConstPessoa();
+pessoa.nome;
+pessoa.idade;
+pessoa.profissao;
+console.log(pessoa.nome);
+console.log(pessoa.idade);
+console.log(pessoa.profissao);
 
 
+// Objeto caneta
+let Caneta = {
+    cor: String,
+    marca: String,
+    modelo: String,
+};
 
+function FuncConstCaneta() {
+    this.cor = "Azul";
+    this.marca = "Bic";
+    this.modelo = "Cristal";
+}
+
+// Chamando a função construtora
+let caneta = new FuncConstCaneta();
+console.log(caneta.cor);
+console.log(caneta.marca);
+console.log(caneta.modelo);
+
+console.log(Caneta.cor);
+console.log(Caneta.marca);
+console.log(Caneta.modelo);
+
+// Objeto fone
+let fone = {
+    marca: "Samsung",
+    modelo: "Galaxy A10",
+    cor: "Preto",
+};
+
+function FuncConstFone() {
+    this.marca = "Samsung";
+    this.modelo = "Galaxy A10";
+    this.cor = "Branco";
+}
+
+console.log(fone.marca);
+console.log(fone.modelo);
+console.log(fone.cor);
+
+// criar um objeto
+let book = {
+    title: "Javascript",
+    author: "Lucas",
+    edition: 7,
+};
+function FunctionConstrutoraBook() {
+    this.title = "Javascript";
+    this.author = "Lucas";
+    this.edition = 7;
+}
+// Função construtora
+let person = {
+    name: "Lucas",
+    age: 20,
+    profession: "Desenvolvedor",
+};
+function FuncaoConstrutora() {
+    this.nome = "Lucas";
+    this.age = 20;
+    this.profissao = "Desenvolvedor";
+}
+/* Caracteristicas
+    - Nome da função começa com letra maiúscula
+    - Usa a palavra-chave this para inicializar propriedades do objeto que está sendo criado
+    - Não usa return
+    - Invocada com o operador new
+
+    Qual a finalidade de uma função construtora? Em que situações ela é utilizada?
+
+    - A finalidade de uma função construtora é criar um novo objeto. Ela é utilizada quando é 
+    necessário criar um novo objeto com as mesmas propriedades e métodos de um objeto já existente.   
+*/
 
 // Funções com código de estrutura de controle
 function abs() {
-    var x = -10;
+    let x = -10;
     if (x < 0) {
         x = -x;
     } else {
@@ -12,9 +128,9 @@ function abs() {
     }
 
     return x;
-} 
-function factorial(n) { 
-    vat product = 1;
+}
+function factorial(n) {
+    let product = 1;
     while (n > 1) {
         product *= n;
         n--;
@@ -24,42 +140,32 @@ function factorial(n) {
 
 factorial(4); // => 24: 1*4*3*2
 
-
 // Javascript é uma linguagem de programação de alto nível, interpretada, orientada a objetos e dinâmica
 Points.dist = function () {
-    var p1 = this[0];
-    var p2 = this[1];
-    var a = p2.x - p1.x;
-    var b = p2.y - p1.y;
+    let p1 = this[0];
+    let p2 = this[1];
+    let a = p2.x - p1.x;
+    let b = p2.y - p1.y;
     return Math.sqrt(a * a + b * b);
 };
 
 // chamando a função
-var p = [
+let p = [
     { x: 0, y: 1 },
     { x: 1, y: 0 },
 ];
 console.log(Points.dist(p));
 
 // Tipos de dados em javascript
-// Números
-// Strings
-// Booleanos
-// Objetos
-// Funções
-// Arrays
-// Data
-// RegExp
-// Null
-// Undefined
+// Números// Strings// Booleanos// Objetos// Funções// Arrays// Data// RegExp// Null// Undefined
 
-var a = []; // cria um array vazio
+let a = []; // cria um array vazio
 a.push(1, 2, 3); // o array agora é [1, 2, 3]
 a.reverse(); // o array agora é [3, 2, 1]
 
 // Funções em javascript
 // Funções são objetos de primeira classe em javascript
-// Funções podem ser atribuídas a variáveis, passadas como argumentos e retornadas de outras funções
+// Funções podem ser atribuídas a letiáveis, passadas como argumentos e retornadas de outras funções
 // Funções podem ser aninhadas, ou seja, uma função pode ser definida dentro de outra
 // Funções podem ser invocadas como métodos
 // Funções podem ser invocadas como construtores
@@ -69,24 +175,24 @@ function square(x) {
 }
 square(2); // => 4
 
-var square = function (x) {
+let square = function (x) {
     return x * x;
 };
-var s = square(2); // s é 4
+let s = square(2); // s é 4
 console.log(s);
 
-// Funções podem ser atribuídas a variáveis, passadas como argumentos e retornadas de outras funções
+// Funções podem ser atribuídas a letiáveis, passadas como argumentos e retornadas de outras funções
 function add(x, y) {
     return x + y;
 }
-var plus = add;
+let plus = add;
 plus(3, 4); // => 7
 
 function div(a, b) {
     return a / b;
 }
-var divide = div;
-var result = divide(10, 2);
+let divide = div;
+let result = divide(10, 2);
 
 if (result == 5) {
     console.log("Resultado correto");
