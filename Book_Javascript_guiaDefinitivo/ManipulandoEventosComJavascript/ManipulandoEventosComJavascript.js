ac6e70bd75cde183e66d6ba2b88bbad4d8c13c6a
Anderson Martins <BlackCode77.1980@gmail.com>
2024-03-10 20:33:24 -0300

    [ JavaScript ] - Maniouladores de eventos do DOM - parte 2

--- Book_Javascript_guiaDefinitivo/ManipulandoEventosComJavascript/ManipulandoEventosComJavascript.js
+++ Book_Javascript_guiaDefinitivo/ManipulandoEventosComJavascript/ManipulandoEventosComJavascript.js
@@ -1,3 +1,9 @@
 const acionarAlert = (value) => {
     alert(value);
 };
+
+/* <button class="class-button">Clique aqui</button> */
+const btn = document.querySelector("button");
+btn.onclick = function alertOne() {
+    alert("Você clicou no botão!");
+};
