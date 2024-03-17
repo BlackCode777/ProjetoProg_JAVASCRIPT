bbd84a75e89e40267cb6416404476c4891a0f28b
Anderson Martins <BlackCode77.1980@gmail.com>
2024-03-17 18:40:31 -0300

    [ Javascript ] - Correção do código com variáveis com erro de referência.

--- Book_Javascript_guiaDefinitivo/OrientacaoObjetosJavaScript/OOClassesHerancaPolimorfismo.js
+++ Book_Javascript_guiaDefinitivo/OrientacaoObjetosJavaScript/OOClassesHerancaPolimorfismo.js
@@ -2,7 +2,12 @@
 
 // Classe
 class CaixaDeFerramenta {
-    constructor() {
+    // Encapsulamento
+    cor;
+    durezaAco;
+    peso;
+
+    constructor(cor, durezaAco, peso) {
         this.cor = cor;
         this.durezaAco = durezaAco;
         this.peso = peso;
@@ -10,13 +15,36 @@ class CaixaDeFerramenta {
     }
 
     adicionar(ferramenta) {
-        this.ferramentas.push(ferramenta, cor, durezaAco, peso);
+        this.ferramentas.push(ferramenta);
     }
 }
 
+// Instanciando a classe
+let caixaDeFerramenta0 = new CaixaDeFerramenta("vermelha", 10, 5);
+let caixaDeFerramenta1 = new CaixaDeFerramenta("azul", 15, 6);
+let caixaDeFerramenta2 = new CaixaDeFerramenta("verde", 20, 7);
+
+// Adicionando ferramentas
+caixaDeFerramenta0.adicionar("chave de fenda");
+caixaDeFerramenta1.adicionar("chave Philips");
+caixaDeFerramenta2.adicionar("chave de boca");
+
+// Exibindo ferramentas
+console.log(caixaDeFerramenta0.ferramentas);
+console.log(caixaDeFerramenta1.ferramentas);
+console.log(caixaDeFerramenta2.ferramentas);
+
 // Herança
 class CaixaDeFerramentaEspecifica extends CaixaDeFerramenta {
-    constructor(cor, durezaAco, peso) {
+    constructor(
+        cor,
+        durezaAco,
+        peso,
+        tipo,
+        descricaoDoUso,
+        fabricante,
+        forcaDeAperto
+    ) {
         // Herda as propriedades da classe CaixaDeFerramente
         super(cor, durezaAco, peso);
         // Adiciona propriedades especificas da classe CaixaDeFerramente
@@ -43,10 +71,14 @@ class CaixaDeFerramentaEspecifica extends CaixaDeFerramenta {
             console.log("Não é uma chave");
         }
     }
+
+    // Método para remover ferramenta
+    remover(ferramenta) {
+        this.ferramentas = this.ferramentas.filter((f) => f !== ferramenta);
+    }
 }
 
 // Instanciando a classe
-let caixaDeFerramenta = new CaixaDeFerramenta("vermelha", 10, 5);
 let caixaDeFerramentaEspecifica = new CaixaDeFerramentaEspecifica(
     "vermelha",
     10,
@@ -58,7 +90,7 @@ let caixaDeFerramentaEspecifica = new CaixaDeFerramentaEspecifica(
 );
 
 // Adicionando ferramentas
-caixaDeFerramenta.adicionar("chave de fenda");
+//caixaDeFerramenta.adicionar("chave de fenda");
 caixaDeFerramentaEspecifica.adicionar("chave de fendaEspecifica");
 
 // Polimorfismo
@@ -75,7 +107,7 @@ caixaDeFerramentaEspecifica.adicionar("Alicate");
 caixaDeFerramentaEspecifica.adicionar("AlicateUniversal");
 
 // Exibindo ferramentas
-console.log(caixaDeFerramenta.ferramentas);
+//console.log(caixaDeFerramenta.ferramentas);
 console.log(caixaDeFerramentaEspecifica.ferramentas);
 console.log(caixaDeFerramentaEspecifica.ferramentas.length);
 console.log(caixaDeFerramentaEspecifica.ferramentas[0]);
@@ -91,3 +123,7 @@ console.log(caixaDeFerramentaEspecifica.ferramentas[9]);
 
 caixaDeFerramentaEspecifica.apertarParafuso();
 caixaDeFerramentaEspecifica.soltarParafuso();
+
+// Comando para rodar o código na linha de comando
+// Entra na pasta onde está o arquivo OOClassesHerancaPolimorfismo.js e executa o comando abaixo
+// $ node OOClassesHerancaPolimorfismo.js
