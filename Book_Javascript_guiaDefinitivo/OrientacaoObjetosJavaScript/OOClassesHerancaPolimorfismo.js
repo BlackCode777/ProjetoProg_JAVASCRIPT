46c6ab0fcc3e6098d3c44a2cdd20bb49827853e1
Anderson Martins <BlackCode77.1980@gmail.com>
2024-03-17 21:21:26 -0300

    [ Javascript ] - Ajuste no método de sobre escrita da Classe CaixaDeFerramentaEspecifica() .

--- Book_Javascript_guiaDefinitivo/OrientacaoObjetosJavaScript/OOClassesHerancaPolimorfismo.js
+++ Book_Javascript_guiaDefinitivo/OrientacaoObjetosJavaScript/OOClassesHerancaPolimorfismo.js
@@ -6,33 +6,16 @@ class CaixaDeFerramenta {
     cor;
     durezaAco;
     peso;
-
     constructor(cor, durezaAco, peso) {
         this.cor = cor;
         this.durezaAco = durezaAco;
         this.peso = peso;
         this.ferramentas = [];
     }
-
     adicionar(ferramenta) {
         this.ferramentas.push(ferramenta);
     }
 }
-
-// Instanciando a classe
-let caixaDeFerramenta0 = new CaixaDeFerramenta("vermelha", 10, 5);
-let caixaDeFerramenta1 = new CaixaDeFerramenta("azul", 15, 6);
-let caixaDeFerramenta2 = new CaixaDeFerramenta("verde", 20, 7);
-
-// Adicionando ferramentas
-caixaDeFerramenta0.adicionar("chave de fenda");
-caixaDeFerramenta1.adicionar("chave Philips");
-caixaDeFerramenta2.adicionar("chave de boca");
-
-// Exibindo ferramentas
-console.log(caixaDeFerramenta0.ferramentas);
-console.log(caixaDeFerramenta1.ferramentas);
-console.log(caixaDeFerramenta2.ferramentas);
 
 // Herança
 class CaixaDeFerramentaEspecifica extends CaixaDeFerramenta {
@@ -53,25 +36,17 @@ class CaixaDeFerramentaEspecifica extends CaixaDeFerramenta {
         this.fabricante = fabricante;
         this.forcaDeAperto = forcaDeAperto;
     }
-
     // metodos especificos da classe CaixaDeFerramentaEspecifica
     apertarParafuso() {
         console.log("Apertando parafuso");
     }
-
     soltarParafuso() {
         console.log("Soltando parafuso");
     }
-
-    // Polimorfismo
+    // Polimorfismo - Método para adicionar ferramenta é sobrescrito para aceitar somente ferramentas do tipo chave
     adicionar(ferramenta) {
-        if (ferramenta.tipo == "chave") {
-            this.ferramentas.push(ferramenta);
-        } else {
-            console.log("Não é uma chave");
-        }
+        this.ferramentas.push(ferramenta);
     }
-
     // Método para remover ferramenta
     remover(ferramenta) {
         this.ferramentas = this.ferramentas.filter((f) => f !== ferramenta);
@@ -79,51 +54,108 @@ class CaixaDeFerramentaEspecifica extends CaixaDeFerramenta {
 }
 
 // Instanciando a classe
-let caixaDeFerramentaEspecifica = new CaixaDeFerramentaEspecifica(
+let caixaFerr = new CaixaDeFerramenta("vermelha", 10, 5);
+let caixaFerrEspec = new CaixaDeFerramentaEspecifica(
     "vermelha",
     10,
     5,
     "chave",
-    "apertar parafuso",
-    "fabricante",
+    "Apertar parafuso",
+    "Tramontina",
     10
 );
 
 // Adicionando ferramentas
-//caixaDeFerramenta.adicionar("chave de fenda");
-caixaDeFerramentaEspecifica.adicionar("chave de fendaEspecifica");
+caixaFerr.adicionar("chave de fenda");
+caixaFerr.adicionar("chave Philips");
+caixaFerr.adicionar("chave de boca");
+console.log(caixaFerr.ferramentas);
 
-// Polimorfismo
-caixaDeFerramentaEspecifica.adicionar("martelo");
-caixaDeFerramentaEspecifica.adicionar("chave de fenda");
-caixaDeFerramentaEspecifica.adicionar("chave de fendaEspecifica");
-caixaDeFerramentaEspecifica.adicionar("chave Philips");
-caixaDeFerramentaEspecifica.adicionar("chave de boca");
-caixaDeFerramentaEspecifica.adicionar("chave de bocaEspecifica");
-caixaDeFerramentaEspecifica.adicionar("chave Ale");
-caixaDeFerramentaEspecifica.adicionar("chave de torque");
-caixaDeFerramentaEspecifica.adicionar("Furadeira");
-caixaDeFerramentaEspecifica.adicionar("Alicate");
-caixaDeFerramentaEspecifica.adicionar("AlicateUniversal");
+caixaFerrEspec.adicionar("chave de fendaEspecifica");
+caixaFerrEspec.adicionar("chave PhilipsEspecifica");
+caixaFerrEspec.adicionar("chave de bocaEspecifica");
+console.log(caixaFerrEspec.ferramentas);
 
 // Exibindo ferramentas
-//console.log(caixaDeFerramenta.ferramentas);
-console.log(caixaDeFerramentaEspecifica.ferramentas);
-console.log(caixaDeFerramentaEspecifica.ferramentas.length);
-console.log(caixaDeFerramentaEspecifica.ferramentas[0]);
-console.log(caixaDeFerramentaEspecifica.ferramentas[1]);
-console.log(caixaDeFerramentaEspecifica.ferramentas[2]);
-console.log(caixaDeFerramentaEspecifica.ferramentas[3]);
-console.log(caixaDeFerramentaEspecifica.ferramentas[4]);
-console.log(caixaDeFerramentaEspecifica.ferramentas[5]);
-console.log(caixaDeFerramentaEspecifica.ferramentas[6]);
-console.log(caixaDeFerramentaEspecifica.ferramentas[7]);
-console.log(caixaDeFerramentaEspecifica.ferramentas[8]);
-console.log(caixaDeFerramentaEspecifica.ferramentas[9]);
-
-caixaDeFerramentaEspecifica.apertarParafuso();
-caixaDeFerramentaEspecifica.soltarParafuso();
 
 // Comando para rodar o código na linha de comando
 // Entra na pasta onde está o arquivo OOClassesHerancaPolimorfismo.js e executa o comando abaixo
 // $ node OOClassesHerancaPolimorfismo.js
+
+/*
+
+// Instanciando a classe
+let caixaDeFerramenta0 = new CaixaDeFerramenta("vermelha", 10, 5);
+let caixaDeFerramenta1 = new CaixaDeFerramenta("azul", 15, 6);
+let caixaDeFerramenta2 = new CaixaDeFerramenta("verde", 20, 7);
+
+// Adicionando ferramentas
+caixaDeFerramenta0.adicionar("chave de fenda");
+caixaDeFerramenta1.adicionar("chave Philips");
+caixaDeFerramenta2.adicionar("chave de boca");
+
+// Exibindo ferramentas
+console.log(caixaDeFerramenta0.ferramentas);
+console.log(caixaDeFerramenta1.ferramentas);
+console.log(caixaDeFerramenta2.ferramentas);
+
+
+console.log();
+console.log("Acessando as ferramentas da caixaDeFerramentaEspecifica");
+console.log(caixaDeFerramentaEspecifica.ferramentas);
+console.log(caixaDeFerramentaEspecifica);
+
+// Exibindo ferramentas
+//console.log(caixaDeFerramenta.ferramentas);
+console.log("");
+console.log(caixaDeFerramentaEspecifica.ferramentas);
+console.log(caixaDeFerramentaEspecifica.ferramentas.length);
+
+console.log();
+console.log("Acessando o objeto caixaDeFerramentaEspecifica");
+console.log(caixaDeFerramentaEspecifica);
+
+console.log();
+console.log("Acessando as propriedades da classe CaixaDeFerramentaEspecifica");
+console.log(caixaDeFerramentaEspecifica.forcaDeAperto);
+console.log(caixaDeFerramentaEspecifica.tipo);
+console.log(caixaDeFerramentaEspecifica.descricaoDoUso);
+console.log(caixaDeFerramentaEspecifica.fabricante);
+console.log(caixaDeFerramentaEspecifica.cor);
+console.log(caixaDeFerramentaEspecifica.durezaAco);
+console.log(caixaDeFerramentaEspecifica.peso);
+console.log(caixaDeFerramentaEspecifica.ferramentas);
+
+caixaDeFerramentaEspecifica.apertarParafuso();
+caixaDeFerramentaEspecifica.soltarParafuso();
+
+
+// console.log(caixaDeFerramentaEspecifica.ferramentas[0]);
+// console.log(caixaDeFerramentaEspecifica.ferramentas[1]);
+// console.log(caixaDeFerramentaEspecifica.ferramentas[2]);
+// console.log(caixaDeFerramentaEspecifica.ferramentas[3]);
+// console.log(caixaDeFerramentaEspecifica.ferramentas[4]);
+// console.log(caixaDeFerramentaEspecifica.ferramentas[5]);
+// console.log(caixaDeFerramentaEspecifica.ferramentas[6]);
+// console.log(caixaDeFerramentaEspecifica.ferramentas[7]);
+// console.log(caixaDeFerramentaEspecifica.ferramentas[8]);
+// console.log(caixaDeFerramentaEspecifica.ferramentas[9]);
+
+// Adicionando ferramentas
+//caixaDeFerramenta.adicionar("chave de fenda");
+// caixaDeFerramentaEspecifica.adicionar("chave de fendaEspecifica");
+
+// Polimorfismo
+// caixaDeFerramentaEspecifica.adicionar("martelo");
+// caixaDeFerramentaEspecifica.adicionar("chave de fenda");
+// caixaDeFerramentaEspecifica.adicionar("chave de fendaEspecifica");
+// caixaDeFerramentaEspecifica.adicionar("chave Philips");
+// caixaDeFerramentaEspecifica.adicionar("chave de boca");
+// caixaDeFerramentaEspecifica.adicionar("chave de bocaEspecifica");
+// caixaDeFerramentaEspecifica.adicionar("chave Ale");
+// caixaDeFerramentaEspecifica.adicionar("chave de torque");
+// caixaDeFerramentaEspecifica.adicionar("Furadeira");
+// caixaDeFerramentaEspecifica.adicionar("Alicate");
+// caixaDeFerramentaEspecifica.adicionar("AlicateUniversal");
+
+*/
